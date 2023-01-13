5227942a00d5be5f76e40ce578310ecf03b0f602
CormacQuigley <122114065+CormacQuigley@users.noreply.github.com>
2023-01-13 20:56:33 +0000

    Update Battle.java
    Got rid of unused variables

--- Java Practice/src/Pokemon/Battle.java
+++ Java Practice/src/Pokemon/Battle.java
@@ -4,24 +4,7 @@ import java.util.Hashtable;
 import java.util.Scanner;
 
 public class Battle {
-	int hp;
-	int AttackDamage;
-	
-	String Type;
-	double Effective = 1.6;
-	double NotEffective = 0.625;
-	//Creating a dictionary which will be filled with moves for each Pokemon type
-    Hashtable<String, Object> attackMoves = new Hashtable<String, Object>();
-    //Create the arrays for the moves
-    String[] waterMoves = {"Hydro Pump", "Surf", "Water Gun"};
-    String[] fireMoves = {"Ember", "Fire Spin", "Fire Fang"};
-    String[] electricMoves = {"Volt Switch","Lightning Bolt","Thunder Fang"};
-    String[] grassMoves = { "Bullet Seed","Vine Whip","Razor Leaf"};
-    /*Create a general array for the attack damage (this will be based on position of moves)
-    These are all 10 right now but as new moves are brought in and current ones are edited
-    this will change
-     */    
-    int[] attackDamage = {10,10,10};
+
 
 	void hpMinus(Pokemon a, Pokemon b) {
 		b.hp = b.hp - a.AttackDamage;
