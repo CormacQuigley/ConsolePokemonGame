0d8aedb9e5b1920f1da8a1375c02682f6c6a3730
CormacQuigley <122114065+CormacQuigley@users.noreply.github.com>
2023-01-13 21:36:54 +0000

    Update Battle.java
    Added feinted check method

--- Java Practice/src/Pokemon/Battle.java
+++ Java Practice/src/Pokemon/Battle.java
@@ -10,23 +10,29 @@ public class Battle {
 		b.hp = b.hp - a.AttackDamage;
 	}
 	
+
+	
 	void startBattle(Pokemon a, Pokemon b) {
 		while(a.hp > 0 && b.hp > 0) {
 			System.out.println("It is your turn!");
 			a.Attack(a);
 			a.typeEffect(a, b);
 			hpMinus(a,b);
+			if(b.feintCheck(b)) { 
+				System.out.println("Your Pokemon has won, you win!");
+				break;
+			}
+		
 			b.showHP();
 			System.out.println("It is your opponents turn!");
 			b.botAttack(b);
 			a.typeEffect(b, a);
 			hpMinus(b,a);
+			if(a.feintCheck(a)) {
+				System.out.println("Your Pokemon has feinted, you lose!");
+				break;
+			}
 			a.showHP();
-		}
-		if(a.hp <= 0) {
-			System.out.println("Your Pokemon has feinted, you lose!");
-		}else {
-			System.out.println("Your Pokemon has won, you win!");
 		}
 	}
 }
